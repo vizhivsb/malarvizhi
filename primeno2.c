aaf25f2a35b794d740bf03e02eebf2d7df10a2b4
vizhivsb <31505964+vizhivsb@users.noreply.github.com>
2018-01-19 16:19:14 +0530

    Create primeno2.c

---
+++ primeno2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,a,count=0;
+	printf("enter the number");
+	scanf("%d",&n);
+	for(i=2;i<n;i++)
+	{
+		a=n%i;
+		if(a==0)
+		{
+		count++;
+		}
+		if((count==0)||(count==1))
+		{
+		printf("%d is prime no",n);
+		}
+	            else
+		{
+		printf("%d is not a prime number",n);
+		}
+                        return 0;
+	}
+	}
+  
