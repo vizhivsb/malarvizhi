a89c2ba400a1a781244e057099669282db933c93
vizhivsb <31505964+vizhivsb@users.noreply.github.com>
2018-01-23 11:24:20 +0000

    Create maxno.c

---
+++ maxno.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))
+printf("a is big");
+if((b>a)&&(b>c))
+printf("b is big);
+if((c>a)&&(c>b))
+printf("c is big);
+}
