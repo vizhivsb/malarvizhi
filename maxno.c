f7dccf3de0583e4b03efa211e779c294ef8d6ed0
vizhivsb <31505964+vizhivsb@users.noreply.github.com>
2018-01-26 16:06:15 +0530

    Update maxno.c

--- maxno.c
+++ maxno.c
@@ -6,7 +6,7 @@ scanf("%d%d%d",&a,&b,&c);
 if((a>b)&&(a>c))
 printf("a is big");
 if((b>a)&&(b>c))
-printf("b is big);
+printf("b is big");
 if((c>a)&&(c>b))
-printf("c is big);
+printf("c is big");
 }
