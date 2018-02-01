2cde4d2120d823e30198981036d7ede8fa63a155
vizhivsb <31505964+vizhivsb@users.noreply.github.com>
2018-02-01 15:15:04 +0530

    Create biggarrayno.c

---
+++ biggarrayno.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int a[5],i,b,c;
+	printf("enter the no");
+	scanf("%d",&a[i]);
+	b=a[0];
+	c=a[0];
+	for(i=1;i<=4;i++)
+	{
+		printf("entr the no");
+		scanf("%d",&a[i]);
+		{
+			if(a[i]>b)
+			b=a[i];
+			else
+			b=0;
+			
+		}
+	printf("the biggest no",b);
+	return 0;
+	}
+}
