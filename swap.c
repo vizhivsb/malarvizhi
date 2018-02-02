3920878a52a884490e65de576ab36b825556f069
vizhivsb <31505964+vizhivsb@users.noreply.github.com>
2018-02-02 15:11:54 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-void swap(int*,int*)
+void swap(int,int);
 void main()
 {
 	int a=10,b=20;
-	printf("before swap values %d%d\n",a,b);
-	swap(&a,&b);
-	printf("after swap values %d%d\n",a,b);
+	printf("before swap values %d %d\n",a,b);
+	swap(a,b);
+	printf("after swap values %d %d\n",a,b);
 }
-int swap(int*x,int*y)
+swap(int x,int y)
 {
-	*x=*x+*y;
-	*y=*x-*y;
-	*x=*x-*y;
-	printf("swap values%d%d\n",*x,*y)
+	x=x+y;
+	y=x-y;
+	x=x-y;
+	printf("swap values%d%d\n",x,y);
 }
