89ac85f1036c9ac72c13d27a667bfb6a78805dc5
vizhivsb <31505964+vizhivsb@users.noreply.github.com>
2018-02-04 12:03:28 +0530

    Create IBMFUNCTION.C

---
+++ IBMFUNCTION.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int sample();
+int main()
+{
+	printf("welcome to vsb\n");
+	sample();
+printf("********\n");
+printf("return to main function\n");
+}
+sample()
+{
+	printf("eee department\n");
+	printf("#######\n");
+}
+
