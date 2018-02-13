495fd06582d46f0cfc7b80e2ebc561c815d92c96
vizhivsb <31505964+vizhivsb@users.noreply.github.com>
2018-02-13 14:52:42 +0530

    Update strcmpwithoutsc.c

--- strcmpwithoutsc.c
+++ strcmpwithoutsc.c
@@ -3,16 +3,14 @@
 int main() {
    char s1[30], s2[30];
    int n,n1;
-   scanf("%s",s1);
-   scanf("%s",s2);
+   scanf("%s",&s1);
+   scanf("%s",&s2);
    n=strlen(s1);
    n1=strlen(s2);
    if (n > n1)
-      printf("%s",s1);
-   else if (n < n1)
-           printf("%s",s2);
+   printf("%s",s1);
+   else 
+    printf("%s",s2);
    else
-      printf("both are equal");
- 
-   return (0);
+    return 0;
 }
